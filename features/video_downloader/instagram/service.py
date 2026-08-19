771582a2ffd597649f3cf84cbb322a673f6fe25c
Akash Developer <pcbackup475@gmail.com>
2026-08-20 00:15:59 +0600

    Add files via upload

--- features/video_downloader/instagram/service.py
+++ features/video_downloader/instagram/service.py
@@ -1,1 +1,3 @@
-
+class InstagramService:
+    """Future platform-specific backend. UI does not depend on implementation."""
+    async def inspect(self,url): return {'title':'Mock Instagram','url':url}
