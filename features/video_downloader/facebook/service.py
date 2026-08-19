06cdfcbe51c937d1b2fecd854c3f59dfacb4b55e
Akash Developer <pcbackup475@gmail.com>
2026-08-20 00:12:30 +0600

    Add files via upload

--- features/video_downloader/facebook/service.py
+++ features/video_downloader/facebook/service.py
@@ -1,1 +1,3 @@
-
+class FacebookService:
+    """Future platform-specific backend. UI does not depend on implementation."""
+    async def inspect(self,url): return {'title':'Mock Facebook','url':url}
