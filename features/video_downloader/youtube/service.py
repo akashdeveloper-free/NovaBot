c94cc17ddd286417b6fab89f437794a98932ac60
Akash Developer <pcbackup475@gmail.com>
2026-08-20 00:17:37 +0600

    Add files via upload

--- features/video_downloader/youtube/service.py
+++ features/video_downloader/youtube/service.py
@@ -1,1 +1,3 @@
-
+class YoutubeService:
+    """Future platform-specific backend. UI does not depend on implementation."""
+    async def inspect(self,url): return {'title':'Mock Youtube','url':url}
